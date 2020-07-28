50435295eff9f960988f4f6f432f2ffaaa0affdb
shraddha-singh <shraddha-singh@ibm.com>
2020-07-28 18:12:35 -0500

    update alert status/severity

--- src/mam/sdk/alerts.py
+++ src/mam/sdk/alerts.py
@@ -50,7 +50,7 @@ query_alerts_schema = {
 
 def get_alerts(json_payload, credentials=None):
     """
-    get alerts ....
+    get alerts using a json payload
     Uses the following APIs:
         POST /api/asengine/v1/<tenant_id>/queryalertdata
 
@@ -107,6 +107,84 @@ def set_alert():
     """
     raise NotImplementedError("In future release")
 
+
 # Get and set Alerts severity, state, action url
 # use post query data for get - will need a json payload
 # use api functions for set - the high value or low value etc
+
+ALLOWED_STATUS_VALUES = ["new", "acknowledged", "resolved", "dismissed"]
+
+
+def update_alert_status(alert_id, new_status, credentials=None):
+    """
+    update alert status using alert id
+
+    :param alert_id str can get alert id using get_alerts
+    :param new_status str
+        allowed values for new_status: New, Acknowledged, Resolved, Dismissed
+    :param credentials dict analytics-service dev credentials
+    :return:
+    """
+    # 1. INPUT CHECKING
+    logger.debug('Performing Input Checking')
+    if new_status.lower() not in ALLOWED_STATUS_VALUES:
+        raise Exception(f'Invalid value {new_status} for new_status argument')
+
+    # 2. API CONNECTION: GET all constants for a tenant
+    logger.debug('Connecting to API')
+    payload = [{
+        "alertId": alert_id,
+        "domainStatus": new_status.lower().capitalize()
+    }]
+    body_arguments = json.dumps(payload).encode('utf-8')
+
+    APIClient.environment_info = generate_api_environment(credentials)
+    # call api to retrieve alerts
+    response = APIClient(api_suffix="alerts",
+                         http_method_name="PUT",
+                         endpoint_suffix="/{orgId}",
+                         body=body_arguments,
+                         ).call_api()
+    if response.status_code != 200:
+        raise Exception('API Client call failed when getting alerts')
+
+    return
+
+
+ALLOWED_SEVERITY_VALUES = ["low", "medium", "medium-high", "high"]
+
+
+def update_alert_severity(alert_id, new_severity, credentials=None):
+    """
+    update alert severity using alert id
+
+    :param alert_id str can get alert id using get_alerts
+    :param new_severity str
+        allowed values for new_severity: Low, Medium, High, Medium-High
+    :param credentials dict analytics-service dev credentials
+    :return:
+    """
+    # 1. INPUT CHECKING
+    logger.debug('Performing Input Checking')
+    if new_severity.lower() not in ALLOWED_SEVERITY_VALUES:
+        raise Exception(f'Invalid value {new_severity} for new_status argument')
+
+    # 2. API CONNECTION: GET all constants for a tenant
+    logger.debug('Connecting to API')
+    payload = [{
+        "alertId": alert_id,
+        "severity": new_severity.lower().capitalize()
+    }]
+    body_arguments = json.dumps(payload).encode('utf-8')
+
+    APIClient.environment_info = generate_api_environment(credentials)
+    # call api to retrieve alerts
+    response = APIClient(api_suffix="alerts",
+                         http_method_name="PUT",
+                         endpoint_suffix="/{orgId}",
+                         body=body_arguments,
+                         ).call_api()
+    if response.status_code != 200:
+        raise Exception('API Client call failed when getting alerts')
+
+    return
